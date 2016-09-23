c070bbd60f5a38e6e74f8c21fd2ac0919c97b19e
Samantha Garcia <samanthamcgarcia@gmail.com>
2016-09-23 20:16:32 -0300

    Add files via upload

---
+++ Panela%2520Fit/src/Objects/RepositorioFornecedor.java
@@ -0,0 +1,68 @@
+package Objects;
+
+import java.util.ArrayList;
+
+public class RepositorioFornecedor {
+	private ArrayList<Fornecedor> listaFornecedores = new ArrayList<Fornecedor>();
+	
+	public ArrayList<Fornecedor> getListaFornecedores() {
+		return listaFornecedores;
+	}
+	
+	public void setListaFornecedores(ArrayList<Fornecedor> listaFornecedores) {
+		this.listaFornecedores = listaFornecedores;
+	}
+	
+	public boolean cadastrarFornecedor(Fornecedor fornecedor) {
+		boolean igual = false;
+		if(fornecedor != null) {
+			if((listaFornecedores.contains(fornecedor))) {
+				return igual;
+			}
+			else {
+				listaFornecedores.add(fornecedor);
+			}
+		}
+		return igual;
+	}
+	
+	public boolean alterarFornecedor(Fornecedor novoFornecedor) {
+		boolean igual = false;
+		if(novoFornecedor != null) {
+			for(int c = 0; c < listaFornecedores.size(); c++) {
+				if(novoFornecedor.getCodigo() == listaFornecedores.get(c).getCodigo()) {
+					listaFornecedores.remove(c);
+					listaFornecedores.add(novoFornecedor);
+					igual = true;
+				}
+			}
+		}
+		return igual;
+	}
+	
+	public Fornecedor buscarFornecedor(int codigo) {
+		for(Fornecedor fornecedor : listaFornecedores) {
+			if(fornecedor.getCodigo() == codigo) {
+				return fornecedor;
+			}
+		}
+		return null;
+	}
+	
+	public boolean removerFornecedor(int codigo) {
+		boolean igual = false;
+		for(Fornecedor fornecedor : listaFornecedores) {
+			if(fornecedor.getCodigo() == codigo) {
+				listaFornecedores.remove(fornecedor);
+				igual = true;
+			}
+		}
+		return igual;
+	}
+	
+	@Override
+	public String toString() {
+		return "RepositorioFornecedor [listaFornecedores = " + listaFornecedores +"]";
+	}
+
+}
